ed28c57dc4513d8d3df0c1e2931eb366c104b312
abdelrahmanahmed <abdelrahman.wahdan@outlook.com>
2020-04-16 12:48:48 +0200

    Updates

---
+++ precache-manifest.d71574d45cc55f0b3207a6d6d9af44fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48dc202e0cc12a5be8e6234cf920f34",
+    "url": "/anything/index.html"
+  },
+  {
+    "revision": "ec7b8cc350ff8e6c33f3",
+    "url": "/anything/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/anything/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anything/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7b8cc350ff8e6c33f3",
+    "url": "/anything/static/js/main.bcda0ce4.chunk.js"
+  },
+  {
+    "revision": "34dbc69170626398ee4d",
+    "url": "/anything/static/js/runtime-main.9ffbf5af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anything/static/media/logo.5d5d9eef.svg"
+  }
+]);
