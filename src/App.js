bb815d2bf1a043b7b73a1fbb47e3d003d1a21007
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-04-18 22:38:03 +0200

    add prettier and format files

--- src/App.js
+++ src/App.js
@@ -1,65 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import Categories from './data/Categories.json';
-import Btn from './components/Btn';
-import { getRandomCategory } from './helpers';
-import './styles/App.css';
+import React, { useState, useEffect } from 'react'
+import Categories from './data/Categories.json'
+import Btn from './components/Btn'
+import { getRandomCategory } from './helpers'
+import './styles/App.css'
 
 function App() {
-  const [categories, setCategories] = useState({});
-  const [category, setCategory] = useState("games");
-  const [result, setResult] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [categories, setCategories] = useState({})
+  const [category, setCategory] = useState('games')
+  const [result, setResult] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   // Fetch Categories data on component initialization
   useEffect(() => {
-    setCategories(Categories);
-  }, []);
+    setCategories(Categories)
+  }, [])
 
   useEffect(() => {
     setCategory(category)
   }, [category])
 
-  const handleCategory = e => {
-    setCategory(e);
+  const handleCategory = (e) => {
+    setCategory(e)
   }
 
   const handleClickMe = () => {
-    setIsLoading(true);
-    let selectedCategory = categories[category].list;
+    setIsLoading(true)
+    let selectedCategory = categories[category].list
     setResult(getRandomCategory(selectedCategory, result))
   }
   return (
     <div className="App">
       <h1 id="title">Bored? &#128580; see what you can do now</h1>
       <div id="categories-container">
-        {Object.keys(categories).map(e => (
-          <span onClick={() => { handleCategory(e) }} key={e} className={category === e ? "tag active" : "tag"}>
+        {Object.keys(categories).map((e) => (
+          <span
+            onClick={() => {
+              handleCategory(e)
+            }}
+            key={e}
+            className={category === e ? 'tag active' : 'tag'}
+          >
             {categories[e].label} {categories[e].emoji}
           </span>
-        )
-        )}
+        ))}
       </div>
 
-      <Btn value={isLoading ? "Loading..." : "Click Me"} onClick={handleClickMe} disabled={isLoading} />
-      {result ?
+      <Btn
+        value={isLoading ? 'Loading...' : 'Click Me'}
+        onClick={handleClickMe}
+        disabled={isLoading}
+      />
+      {result ? (
         <div className="result">
           <div className="card">
-            {isLoading ? <div className="lds-hourglass"></div> :
+            {isLoading ? (
+              <div className="lds-hourglass"></div>
+            ) : (
               <div className="container">
-                <h4><b>{result.name}</b></h4>
+                <h4>
+                  <b>{result.name}</b>
+                </h4>
               </div>
-            }
+            )}
 
             <img
               style={isLoading ? { display: 'none' } : {}}
-              onLoad={() => { setIsLoading(false) }}
-              src={result.url} alt={result.name} />
+              onLoad={() => {
+                setIsLoading(false)
+              }}
+              src={result.url}
+              alt={result.name}
+            />
           </div>
-
         </div>
-        : null}
+      ) : null}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,19 @@
 const getRandomCategory = (selectedCategory, currentCategory) => {
-    let output;
-    let randomIndex;
-    while (true) {
-        randomIndex = Math.floor(Math.random() * Math.floor(selectedCategory.length));
-        if (JSON.stringify(selectedCategory[randomIndex]) !== JSON.stringify(currentCategory)) {
-            output = selectedCategory[randomIndex];
-            break;
-        }
+  let output
+  let randomIndex
+  while (true) {
+    randomIndex = Math.floor(
+      Math.random() * Math.floor(selectedCategory.length)
+    )
+    if (
+      JSON.stringify(selectedCategory[randomIndex]) !==
+      JSON.stringify(currentCategory)
+    ) {
+      output = selectedCategory[randomIndex]
+      break
     }
-    return output;
+  }
+  return output
 }
 
-export {
-    getRandomCategory
-}
+export { getRandomCategory }
